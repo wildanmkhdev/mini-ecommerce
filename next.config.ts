ffe4788cc2812947c7ff8cc68ed3043f3dcbcad8
wildanmkhdev <wildankhalidun97@gmail.com>
2025-09-08 00:05:45 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	turbopack: {
+		root: __dirname, // pastikan sesuai root project kamu
+	},
 };
 
 export default nextConfig;
